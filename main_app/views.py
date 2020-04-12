88d63a155b3d581a4b760e51b363724ff792866a
Vincent Robles <sylkisgsx@gmail.com>
2020-04-12 12:29:38 -0400

    added "about" view

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+
+
+def home(request):
+    return HttpResponse('<h1>Hello<h1>')
+
+
+def about(request):
+    return HttpResponse('<h1>About Page</h1>')
