aabd59113213d1595ef8094712f9e604fc214096
Vincent Robles <sylkisgsx@gmail.com>
2020-04-13 01:41:31 -0400

    created pokemon model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Pokemon(models.Model):
+    name = models.CharField(max_length=100)
+    pokemon_type = models.CharField(max_length=100)
+    weakness = models.CharField(max_length=50)
+    lvl = models.IntegerField()
+    description = models.TextField(max_length=250)
